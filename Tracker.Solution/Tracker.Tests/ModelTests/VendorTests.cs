cddcfc7455134c4eac6d71fd56fcb121e8cf8011
Mikerophonic <micgcon@gmail.com>
2023-09-23 13:58:56 -0700

    write vendor test 3, set address

--- Tracker.Solution/Tracker.Tests/ModelTests/VendorTests.cs
+++ Tracker.Solution/Tracker.Tests/ModelTests/VendorTests.cs
@@ -12,7 +12,7 @@ namespace Tracker.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("Name");
+      Vendor newVendor = new Vendor("Name", "Address");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
 
@@ -23,11 +23,27 @@ namespace Tracker.Tests
       string name = "Suzie's Cafe";
 
       //Act
-      Vendor newVendor = new Vendor(name);
+      Vendor newVendor = new Vendor(name, "Address");
       string result = newVendor.Name;
 
       //Assert
       Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void SetAddress_SetAddress_String()
+    {
+      //Arrange
+      string address = "13050 Borden Ave";
+      Vendor newVendor = new Vendor("Suzie's Cafe", address);
+
+      //Act
+      string updatedAddress = "23050 Borden Blvd";
+      newVendor.Address = updatedAddress;
+      string result = newVendor.Address;
+
+      //Assert
+      Assert.AreEqual(updatedAddress, result);
     }
 
   }
