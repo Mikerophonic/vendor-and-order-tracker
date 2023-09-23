6b27ec1c2475cc8310c84e60756a25a3e11b5356
Mikerophonic <micgcon@gmail.com>
2023-09-23 13:47:32 -0700

    write test two, get name

--- Tracker.Solution/Tracker.Tests/ModelTests/VendorTests.cs
+++ Tracker.Solution/Tracker.Tests/ModelTests/VendorTests.cs
@@ -12,8 +12,22 @@ namespace Tracker.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor();
+      Vendor newVendor = new Vendor("Name");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "Suzie's Cafe";
+
+      //Act
+      Vendor newVendor = new Vendor(name);
+      string result = newVendor.name;
+
+      //Assert
+      Assert.AreEqual(name, result);
     }
 
   }
